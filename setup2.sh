b1e6d256b940abadc8e13d3106dd1d0b49636e68
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-10-17 00:39:04 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -rf c4_deployment-5
+rm -rf Deployment5
 source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
+git clone https://github.com/DANNYDEE93/Deployment5.git
+cd Deployment5
 pip install -r requirements.txt
 pip install gunicorn
 python database.py
